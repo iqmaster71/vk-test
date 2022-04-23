ba5d4e85f8d22213612087932a2777ebd5a61b89
iqmaster71 <pomor1971@mail.ru>
2022-04-23 14:04:09 +0300

    fix

--- src/test/kotlin/ru/netology/MainKtTest.kt
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -1,64 +1,9 @@
 package ru.netology
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.Assert.*
 
 class MainKtTest {
-
-    @Test
-    fun calcCommissionOfTransfer_VkPayZero() {
-        val typeOfCard = TYPE_CARD_VK_PAY
-        val amountOfPreviousMonthPeriod = 0
-        val transferAmount = 14_000_00
-
-        val result = calcCommissionOfTransfer(typeOfCard, amountOfPreviousMonthPeriod, transferAmount)
-
-        // assertEquals(0, result)
-    }
-
-    @Test
-    fun calcCommissionOfTransfer_VkPayOneTimeLimit() {
-        val typeOfCard = TYPE_CARD_VK_PAY
-        val amountOfPreviousMonthPeriod = 0
-        val transferAmount = 16_000_00
-
-        val result = calcCommissionOfTransfer(typeOfCard, amountOfPreviousMonthPeriod, transferAmount)
-
-        // assertEquals(0, result)
-    }
-
-    @Test
-    fun calcCommissionOfTransfer_VkPayMonthLimit() {
-        val typeOfCard = TYPE_CARD_VK_PAY
-        val amountOfPreviousMonthPeriod = 39_000_00
-        val transferAmount = 11_000_00
-
-        val result = calcCommissionOfTransfer(typeOfCard, amountOfPreviousMonthPeriod, transferAmount)
-
-        // assertEquals(0, result)
-    }
-
-    @Test
-    fun calcCommissionOfTransfer_AnyCardMonthLimit() {
-        val typeOfCard = TYPE_CARD_MIR
-        val amountOfPreviousMonthPeriod = 599_000_00
-        val transferAmount = 11_000_00
-
-        val result = calcCommissionOfTransfer(typeOfCard, amountOfPreviousMonthPeriod, transferAmount)
-
-        // assertEquals(0, result)
-    }
-
-    @Test
-    fun printMassageOfTransfer_Easy() {
-        val typeOfCard = TYPE_CARD_VK_PAY
-        val transferAmount = 16_000
-        val commissionOfTransfer = 0
-
-        printMassageOfTransfer(typeOfCard, transferAmount, commissionOfTransfer)
-
-        //assertEquals(0, result)
-    }
 
     @Test
     fun calcFullCommissionOfTransfer_TestVKPay() {
